957ed5e62d1053fb3576c1b2f4ba63fc05d28a11
Per Andersen <per.j.andersen@gmail.com>
2011-04-29 08:17:35 +0000

    cleaned up binaries...

--- src/HTTPHandler.java
+++ src/HTTPHandler.java
@@ -3,6 +3,7 @@ import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -10,21 +11,30 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.util.EntityUtils;
-
+import org.apache.*;
+import org.apache.http.*;
+import org.*;
 
 import java.util.regex.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
 
 public class HTTPHandler {
 	
 	public static void main(String args[]) throws ClientProtocolException, IOException{
-		kistalkLogin("http://kistalk.com/login/create", "foo", "bar");
+		kistalkLogin("http://kistalk.com/login/create", "panderse", "");
 	}
 	
 	public HTTPHandler() {
@@ -54,20 +64,37 @@ public class HTTPHandler {
 	}
 	
 	
-	public static void kistalkLogin(String loginUrl, String username, String password) {
+	public static void kistalkLogin(String loginUrl, String username, String password) throws ClientProtocolException, IOException {
 
 		String loginPage = httpGet(loginUrl);
 		String myPat = 	"(<input\\stype=\"hidden\"\\sname=\"lt\"\\svalue=\")(.{76})(\"/>)";
 
 		Pattern p = Pattern.compile(myPat);
 		Matcher m = p.matcher(loginPage);
-
-		if(m.matches()){
-			System.out.println(m.group(1));
+		String key = null;
+		
+		if(m.find()){
+			key = m.group(2);
+		} else {
+			throw new RuntimeException("Authentication error: Failed to retrieve CAS-Key.");
 		}
-
-		while(m.find()){
-			System.out.print(loginPage.substring(m.start(), m.end()));
-		}
+		HttpClient client = new DefaultHttpClient();
+		CookieStore cookieJar = new BasicCookieStore();
+		
+		HttpPost request = new HttpPost("https://login.kth.se/login?service=http%3A%2F%2Fkistalk.com%2Flogin%2Fcreate");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		
+		params.add(new BasicNameValuePair("_eventId", "submit"));
+		params.add(new BasicNameValuePair("lt", key));
+		params.add(new BasicNameValuePair("usedMethod", "password"));
+		params.add(new BasicNameValuePair("username", username));
+		params.add(new BasicNameValuePair("password", password));
+		
+		UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params);
+		request.setEntity(formEntity);
+		
+		HttpResponse response = client.execute(request);
+		
+		System.out.println(response);
 	}
 }
