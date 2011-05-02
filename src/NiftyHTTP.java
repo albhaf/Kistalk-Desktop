8cb73dd5abab1a0eb8085d53f13642d3225877e6
Per Andersen <per.j.andersen@gmail.com>
2011-05-02 12:08:56 +0000

    added method to retrieve XML-feed via NiftyHTTP

--- src/NiftyHTTP.java
+++ src/NiftyHTTP.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.*;
 import java.util.ArrayList;
@@ -21,6 +25,7 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.BasicResponseHandler;
@@ -36,6 +41,7 @@ import org.*;
 public class NiftyHTTP {
 	
 	final static String AUTH_URL = "http://kistalk.com/api/validate_token";
+	final static String FEED_URL = "http://kistalk.com/api/feed/android.xml";
 	
 	private String username;
 	private String authToken;
@@ -58,21 +64,87 @@ public class NiftyHTTP {
 		
 		NiftyHTTP n = new NiftyHTTP("panderse", "emgkxra2it");
 		
+		System.out.println(n.getXMLFeed());
 	}
 	
 	public boolean validateToken() {
-		HttpClient client = new DefaultHttpClient();
-		HttpPost request = new HttpPost(AUTH_URL);
+		
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		
 		params.add(new BasicNameValuePair("username", this.username));
 		params.add(new BasicNameValuePair("token", this.authToken));
 		
+		return this.simplePost(AUTH_URL, params).trim().equals("true");
+	}
+	
+	public String getXMLFeed() {
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		
+		params.add(new BasicNameValuePair("username", this.username));
+		params.add(new BasicNameValuePair("token", this.authToken));
+		
+		return this.simpleGet(FEED_URL, params);
+	}
+	
+	private String simpleGet(String URL) {
+		return this.simpleGet(URL, null);
+	}
+	
+	private String simpleGet(String URL, List<NameValuePair> params) {
+		
+		HttpClient client = new DefaultHttpClient();
+
+		
+		if(params != null){
+			URL = URL + "?";
+			Iterator<NameValuePair> it = params.iterator();
+			
+			while(it.hasNext()) {
+				NameValuePair nvp = it.next();
+				try {
+					URL = URL + URLEncoder.encode(nvp.getName(), "UTF-8") + "=" + URLEncoder.encode(nvp.getValue(), "UTF-8");
+					if (it.hasNext()){
+						URL = URL + "&";
+					}
+				} catch (UnsupportedEncodingException e) {
+					System.err.print("Failed to encode url for GET request: ");
+					System.err.println(e.getStackTrace());
+				}
+			}
+		}
+		
+		System.out.println(URL);
+		
+		HttpGet request = new HttpGet(URL);
+		
+		ResponseHandler<String> brs = new BasicResponseHandler();
+		
+		String response = "false";
+		
+		try {
+			response = client.execute(request, brs);
+		} catch (Exception e) {
+			System.err.print("Unable to retrieve XML-Feed: ");
+			System.err.println(e.getStackTrace());
+		} finally {
+			client.getConnectionManager().shutdown();
+		}
+
+		return response;
+		
+	}
+	
+	
+	private String simplePost(String URL, List<NameValuePair> params) {
+		
+		HttpClient client = new DefaultHttpClient();
+		HttpPost request = new HttpPost(URL);
+		
 		try {
 			UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params);
 			request.setEntity(formEntity);
 		} catch (UnsupportedEncodingException e){
-			System.err.print("Failed to encode authentication parameters: ");
+			System.err.print("Failed to encode parameters while attempting to craft post request: ");
 			System.err.println(e.getStackTrace());
 		}
 		
@@ -83,16 +155,12 @@ public class NiftyHTTP {
 		try {
 			response = client.execute(request, brs);
 		} catch (Exception e) {
-			System.err.print("Unable to reach server for authentication of token: ");
+			System.err.print("Unable to retrieve XML-Feed: ");
 			System.err.println(e.getStackTrace());
 		} finally {
 			client.getConnectionManager().shutdown();
 		}
-		
-		return (response.trim().equals("true"));
-	}
-	
-	public String getDesktopXML() {
-		return "ye aulde xml feede";
+
+		return response;
 	}
 }
