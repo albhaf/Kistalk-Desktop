15b8e3fcbd49aee5f7ae988eae0df2e1e6e5e7f1
Per Andersen <per.j.andersen@gmail.com>
2011-05-03 09:25:05 +0000

    ?

--- src/NiftyHTTP.java
+++ src/NiftyHTTP.java
@@ -8,8 +8,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,17 +36,35 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.util.EntityUtils;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.*;
 import org.apache.http.*;
 import org.*;
+import java.util.Map.Entry;
 
 public class NiftyHTTP {
 	
 	final static String AUTH_URL = "http://kistalk.com/api/validate_token";
 	final static String FEED_URL = "http://kistalk.com/api/feed/desktop.xml";
+	final static String ANNOUNCEMENTS_URL = "http://kistalk.com/api/announcement/new";
+	final static String ENCODING = "UTF-8";
 	
 	private String username;
 	private String authToken;
+	
+	public static void main(String[] args){
+		
+		NiftyHTTP nh = new NiftyHTTP("panderse", "z0vu7iegcn");
+		
+		HashMap<String, String> hm = new HashMap<String, String>();
+		
+		hm.put("pub_open", "1");
+		hm.put("food_ready", "0");
+		hm.put("food_description", "kött");
+		hm.put("event", "KÖTT FÖ FÅÄÄEEEEEN");
+		
+		System.out.print(nh.postAnnouncement(hm));
+	}
 	
 	public NiftyHTTP (String username, String authToken) throws RuntimeException {
 		
@@ -58,13 +78,38 @@ public class NiftyHTTP {
 			System.err.println(e.getStackTrace());
 		}
 	}
+
+	public String postAnnouncement(HashMap<String, String> announcements) {
+		
+		/*
+		 * Typically, these announcements can be posted:
+		 * pub_open ( = 0 or 1), food_ready ( = 0 or 1), food_description, event
+		 */
+		
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+		params.add(new BasicNameValuePair("utf-8", "✓"));
+		params.add(new BasicNameValuePair("username", this.username));
+		params.add(new BasicNameValuePair("token", this.authToken));
+		
+		for (Entry<String, String> e : announcements.entrySet()){
+			params.add(new BasicNameValuePair(e.getKey(), e.getValue()));
+		}
+		
+		return this.simplePost(ANNOUNCEMENTS_URL, params);
+	}
 	
 	private boolean validateToken() {
 		
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		
-		params.add(new BasicNameValuePair("username", this.username));
-		params.add(new BasicNameValuePair("token", this.authToken));
+		try {
+			params.add(new BasicNameValuePair("username", URLEncoder.encode(this.username, ENCODING)));
+			params.add(new BasicNameValuePair("token", URLEncoder.encode(this.authToken, ENCODING)));
+		} catch (UnsupportedEncodingException e) {
+			System.err.print("Failed to encode url for POST request: ");
+			e.printStackTrace();
+		}
 		
 		return this.simplePost(AUTH_URL, params).trim().equals("true");
 	}
@@ -72,10 +117,19 @@ public class NiftyHTTP {
 	public String getXMLFeed() {
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		
-		params.add(new BasicNameValuePair("username", this.username));
-		params.add(new BasicNameValuePair("token", this.authToken));
+		try {
+			params.add(new BasicNameValuePair("username", URLEncoder.encode(this.username, ENCODING)));
+			params.add(new BasicNameValuePair("token", URLEncoder.encode(this.authToken, ENCODING)));
+		} catch (UnsupportedEncodingException e) {
+			System.err.print("Failed to encode url for POST request: ");
+			e.printStackTrace();
+		}
 		
 		return this.simpleGet(FEED_URL, params);
+	}
+	
+	public String getImage(String URL) {
+		return "foo";
 	}
 	
 	private String simpleGet(String URL) {
@@ -86,26 +140,25 @@ public class NiftyHTTP {
 		
 		HttpClient client = new DefaultHttpClient();
 
-		
 		if(params != null){
 			URL = URL + "?";
 			Iterator<NameValuePair> it = params.iterator();
 			
 			while(it.hasNext()) {
+				
 				NameValuePair nvp = it.next();
+				
 				try {
-					URL = URL + URLEncoder.encode(nvp.getName(), "UTF-8") + "=" + URLEncoder.encode(nvp.getValue(), "UTF-8");
+					URL = URL + URLEncoder.encode(nvp.getName(), ENCODING) + "=" + URLEncoder.encode(nvp.getValue(), ENCODING);
 					if (it.hasNext()){
 						URL = URL + "&";
 					}
 				} catch (UnsupportedEncodingException e) {
 					System.err.print("Failed to encode url for GET request: ");
-					System.err.println(e.getStackTrace());
+					e.printStackTrace();
 				}
 			}
 		}
-		
-		System.out.println(URL);
 		
 		HttpGet request = new HttpGet(URL);
 		
@@ -117,38 +170,37 @@ public class NiftyHTTP {
 			response = client.execute(request, brs);
 		} catch (Exception e) {
 			System.err.print("Unable to retrieve XML-Feed: ");
-			System.err.println(e.getStackTrace());
+			e.printStackTrace();
 		} finally {
 			client.getConnectionManager().shutdown();
 		}
 
 		return response;
-		
 	}
-	
 	
 	private String simplePost(String URL, List<NameValuePair> params) {
 		
 		HttpClient client = new DefaultHttpClient();
 		HttpPost request = new HttpPost(URL);
 		
+		request.setHeader("ContentType", "application/x-www-form-urlencoded;charset=utf-8;");
+		
 		try {
 			UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params);
 			request.setEntity(formEntity);
 		} catch (UnsupportedEncodingException e){
 			System.err.print("Failed to encode parameters while attempting to craft post request: ");
-			System.err.println(e.getStackTrace());
+			e.printStackTrace();
 		}
 		
 		ResponseHandler<String> brs = new BasicResponseHandler();
-		
-		String response = "false";
+		String response = null;
 		
 		try {
 			response = client.execute(request, brs);
 		} catch (Exception e) {
-			System.err.print("Unable to retrieve XML-Feed: ");
-			System.err.println(e.getStackTrace());
+			System.err.print("Unable execute POST request: ");
+			e.printStackTrace();
 		} finally {
 			client.getConnectionManager().shutdown();
 		}
