124a217369cb383b8c60d22d6c52d0434e108f54
Ludvig Cederfelt <ludvigce@kth.se>
2011-05-03 08:32:53 +0200

    blablblalsdlaldakdlamdalsndmla

--- src/ImportPubSlides.java
+++ src/ImportPubSlides.java
@@ -1,4 +1,5 @@
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -31,6 +32,11 @@ public class ImportPubSlides {
 	public BufferedImage getImage(int i) throws IOException{
 		Image img = ImageIO.read(new File("slide-" + i + ".hansimage"));
 		return (BufferedImage) img;
+	}
+	
+	public static void main(String a[]) throws IOException {
+		ImportPubSlides blah = new ImportPubSlides("C:\\Users\\Ludvig\\Documents\\asd.ppt");
+		blah.getImage(0);
 	}
 	
 	}
