25bbc471d985f35bdbc9fa4d349dc46308764eef
Ludvig Cederfelt <ludvigce@kth.se>
2011-05-04 15:13:35 +0200

    tar bort /images n'r programmet st'ngs av

--- src/TwoDSlideShowView.java
+++ src/TwoDSlideShowView.java
@@ -4,20 +4,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class TwoDSlideShowView {
+public class TwoDSlideShowView  {
 	JPanel panel;
 
 	protected TwoDSlideShowView() {
 	}
 
-	protected void createFrame(ShowImage slideShowHandler,
-			Rectangle monitor) {
+	protected void createFrame(ShowImage slideShowHandler, Rectangle monitor) {
 		JFrame frame = new JFrame("ShowImage.java");
 		
 		panel = slideShowHandler;
 		frame.getContentPane().add(panel);
 
-		frame.setUndecorated(true);
+		FrameListener wl = new FrameListener();
+		frame.addWindowListener(wl);
+		//frame.setUndecorated(true);
 		frame.setSize(monitor.width, monitor.height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
