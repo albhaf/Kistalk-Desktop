143998a72bd95402862ec2ac5eea983e6c478b09
Ludvig Cederfelt <ludvigce@kth.se>
2011-05-04 17:14:52 +0200

    St'ng av slideshow

--- src/TwoDSlideShowView.java
+++ src/TwoDSlideShowView.java
@@ -6,22 +6,26 @@ import javax.swing.JPanel;
 
 public class TwoDSlideShowView  {
 	JPanel panel;
+	JFrame frame;
 
 	protected TwoDSlideShowView() {
 	}
 
 	protected void createFrame(ShowImage slideShowHandler, Rectangle monitor) {
-		JFrame frame = new JFrame("ShowImage.java");
+		frame = new JFrame("ShowImage.java");
+		FrameListener wl = new FrameListener();
 		
 		panel = slideShowHandler;
 		frame.getContentPane().add(panel);
-
-		FrameListener wl = new FrameListener();
 		frame.addWindowListener(wl);
 		frame.setUndecorated(true);
 		frame.setSize(monitor.width, monitor.height);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setVisible(true);
+	}
+	
+	public void terminate(){
+		frame.dispose();
 	}
 	
 	
