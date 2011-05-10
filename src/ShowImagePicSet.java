4e886fb3d4f82d1fa1ad2b42f2972af3df808ab2
Ludvig Cederfelt <ludvigce@kth.se>
2011-05-10 09:29:11 +0200

    logo uppe till v'nster endast d[ bilder fr[n kistalkt.com

--- src/ShowImagePicSet.java
+++ src/ShowImagePicSet.java
@@ -1,10 +1,6 @@
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 
 public class ShowImagePicSet {
